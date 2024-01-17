416a270b1247dedcd617effd7e810ab1e159a6be
Jannis R <mail@jannisr.de>
2024-01-17 20:51:13 +0100

    build script: tweak download command

--- build/download.sh
+++ build/download.sh
@@ -7,7 +7,9 @@ cd $(dirname $0)
 base_url='https://vbb-gtfs.jannisr.de/latest/'
 # todo: use https://gist.github.com/derhuerst/745cf09fe5f3ea2569948dd215bbfe1a ?
 download () {
-	curl -L --compressed --etag-compare "$1.etag" --etag-save "$1.etag" "$base_url$1" -o "$1"
+	curl -fsSL --compressed -H 'User-Agent: derhuerst/vbb-stations build' \
+		--etag-compare "$1.etag" --etag-save "$1.etag" \
+		"$base_url$1" -o "$1"
 }
 
 download 'stops.csv'
