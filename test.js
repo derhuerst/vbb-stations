af073d74296b7fb995bfa6fa3d5c55277141f2ac
Jannis R <mail@jannisr.de>
2026-05-11 16:43:42 +0200

    tape -> node:test ✅

--- test.js
+++ test.js
@@ -1,4 +1,8 @@
-import test from 'tape'
+import {test} from 'node:test'
+import {
+	strictEqual,
+	ok,
+} from 'node:assert/strict'
 import isRoughlyEqual from 'is-roughly-equal'
 import shorten from 'vbb-short-station-name'
 
@@ -8,95 +12,86 @@ import full from './full.json' with {type: 'json'}
 
 
 test('filterById', (t) => {
-	t.plan(6)
 	const p = filterById(2)
 
-	t.equal(p(), false)
-	t.equal(p({}), false)
-	t.equal(p([2]), false)
-	t.equal(p({id: 1}), false)
-	t.equal(p({id: '2'}), false)
-	t.equal(p({id: 2}), true)
+	strictEqual(p(), false)
+	strictEqual(p({}), false)
+	strictEqual(p([2]), false)
+	strictEqual(p({id: 1}), false)
+	strictEqual(p({id: '2'}), false)
+	strictEqual(p({id: 2}), true)
 })
 
 test('filterByKeys', (t) => {
 	t.test('returns false for invalid data', (t) => {
-		t.plan(3)
 		const p = filterByKeys({a: 'foo'})
 
-		t.equal(p(), false)
-		t.equal(p([2]), false)
-		t.equal(p({}), false)
+		strictEqual(p(), false)
+		strictEqual(p([2]), false)
+		strictEqual(p({}), false)
 	})
 
 	t.test('compares strictly', (t) => {
-		t.plan(2)
 		const p = filterByKeys({a: '1'})
 
-		t.equal(p({a: 1}), false)
-		t.equal(p({a: '1'}), true)
+		strictEqual(p({a: 1}), false)
+		strictEqual(p({a: '1'}), true)
 	})
 
 	t.test('compares multiple keys', (t) => {
-		t.plan(4)
 		const p = filterByKeys({a: 'foo', b: 'bar'})
 
-		t.equal(p({a: 'bar', b: 'bar'}), false)
-		t.equal(p({a: 'bar', b: 'foo'}), false)
-		t.equal(p({a: 'foo', b: 'foo'}), false)
-		t.equal(p({a: 'foo', b: 'bar'}), true)
+		strictEqual(p({a: 'bar', b: 'bar'}), false)
+		strictEqual(p({a: 'bar', b: 'foo'}), false)
+		strictEqual(p({a: 'foo', b: 'foo'}), false)
+		strictEqual(p({a: 'foo', b: 'bar'}), true)
 	})
 
 	t.test('compares deeply', (t) => {
-		t.plan(2)
 		const p = filterByKeys({'a.b': 'foo'})
 
-		t.equal(p({a: {b: 'foo'}}), true)
-		t.equal(p({'a.b': 'foo'}), true)
+		strictEqual(p({a: {b: 'foo'}}), true)
+		strictEqual(p({'a.b': 'foo'}), true)
 	})
 })
 
 test('filters correctly', (t) => {
-	t.plan(2)
 	const data = stations({
 		id: 'de:11000:900009101', // U Amrumer Str.
 		'location.latitude': 52.542202
 	})
 
-	t.equal(data.length, 1)
-	t.equal(data[0].id,  'de:11000:900009101')
+	strictEqual(data.length, 1)
+	strictEqual(data[0].id,  'de:11000:900009101')
 })
 
 test('contains shortened station names', (t) => {
-	t.plan(2)
 	const [amrumerStr] = stations('de:11000:900009101') // U Amrumer Str.
 
-	t.ok(amrumerStr)
-	if (amrumerStr) t.equal(amrumerStr.name, shorten(amrumerStr.name))
+	ok(amrumerStr)
+	if (amrumerStr) strictEqual(amrumerStr.name, shorten(amrumerStr.name))
 })
 
 test('full', (t) => {
-	t.plan(11)
 	const s = full['de:11000:900009101'] // U Amrumer Str.
 
-	t.equal(s.type, 'station')
-	t.equal(s.id, 'de:11000:900009101')
-	t.equal(s.name.slice(0, 14), 'U Amrumer Str.')
-	t.ok(s.location)
-	t.equal(s.location.type, 'location')
-	t.equal(s.location.latitude, 52.542202)
-	t.equal(s.location.longitude, 13.349534)
-	t.ok(isRoughlyEqual(2500, s.weight, 4000))
-	t.ok(Array.isArray(s.stops))
+	strictEqual(s.type, 'station')
+	strictEqual(s.id, 'de:11000:900009101')
+	strictEqual(s.name.slice(0, 14), 'U Amrumer Str.')
+	ok(s.location)
+	strictEqual(s.location.type, 'location')
+	strictEqual(s.location.latitude, 52.542202)
+	strictEqual(s.location.longitude, 13.349534)
+	ok(isRoughlyEqual(2500, s.weight, 4000))
+	ok(Array.isArray(s.stops))
 	// todo: actually it's 7, but the VBB GTFS data has a bug
-	// t.equal(s.stops.length, 7)
-	t.equal(s.stops.length, 2)
+	// strictEqual(s.stops.length, 7)
+	strictEqual(s.stops.length, 2)
 
-	t.test('every station has an id', (t) => {
+	{
 		for (let id in full) {
 			const s = full[id]
-			t.equal(s.id, id, id + ' has no id')
+			strictEqual(s.id, id, id + ' has no id')
 		}
-		t.end()
-	})
+	}
 })
